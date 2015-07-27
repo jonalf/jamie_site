8b836cdc122792a6cf9b3a877e0487e3659f58c4
JonAlf <dw@stuycs.org>
2015-07-27 11:21:01 -0400

    made freezer useable locally and on fry

--- new-site/site_freezer.py
+++ new-site/site_freezer.py
@@ -1,16 +1,14 @@
 from flask_frozen import Freezer
 from app import site
+from socket import gethostname
 
+if gethostname() == 'fry':
+    BASE_URL = 'http://fry.jonalf.com/jamie_site_live'
+else:
+    BASE_URL = 'file:///Users/jdyrlandweaver/github/jamie_site_live'
+    
 site.config['FREEZER_DESTINATION'] = '../../jamie_site_live/'
-site.config['FREEZER_BASE_URL'] = 'file:///Users/jdyrlandweaver/github/jamie_site_live'
-#site.config['SERVER_NAME'] = 'file:///Users/jdyrlandweaver/github/jamie_site_live'
-
-"""
-site.add_url_rule('/static/<path:filename>',
-                 endpoint='static',
-                 subdomain='static',
-                 view_func=site.send_static_file)
-"""
+site.config['FREEZER_BASE_URL'] = BASE_URL
 
 freezer = Freezer( site )
 
